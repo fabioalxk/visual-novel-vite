66602506e279373f7bd7705b867328c7d79e239f
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2025-06-26 21:24:46 -0300

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -4,5 +4,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
+console.log("API Base URL:", api.defaults.baseURL);
 
 export default api;
